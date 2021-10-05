3065e521e849e24b5e403ed1074d44624220fab9
Alejandro <borsal_ino@hotmail.com>
2021-10-04 20:11:33 -0500

    trabajo del algoritmo de euclides

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include "iostream"
+using namespace std;
+// Alejandro joel gonzales Huamani
+
+void euclides(int a,int b,int &c){
+    int aux;int r = 0;
+    if(a>b){
+        aux = a;
+        a = b;
+        b = aux;
+    }
+    for(int i = 1;i<a/2;i++){
+        if(a%i == 0 && b%i == 0)
+            r=i;
+    }
+    c = r;
+}
+void euclidesGeneralizado(int a,int b,int &MDC,int &x,int &y){
+    if(b==0){
+        MDC=a;
+        x = 1;
+        y = 0;
+    }
+    else{
+        euclidesGeneralizado(b,a%b,MDC,x,y);
+        int aux = x;
+        aux = x;
+        x = y;
+        y = aux-(a/b)*y; 
+    }
+}
+
+int main(){
+    int a,b,mcd=0,x=0,y=0;
+    cout<<"escriba el primer numero: ";
+    cin >>a;cout<<"escriba el segundo numero : ";
+    cin>>b;
+    //euclides(a,b,mcd);
+
+    // a*x +b*y = 1 
+    euclidesGeneralizado(a,b,mcd,x,y);
+    cout<<"\nprimer numero: "<<a<<" segundo numero: "<<b<<" el MCD es: "<<mcd<<" x:"<<x<<" y:"<<y;
+    
+    return 0;
+}
